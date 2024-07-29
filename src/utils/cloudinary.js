0f3ac9fbef7de447ca28fa6ca69beb168c2eeb21
Utkarsh Garg <utkarsh.garg@unilever.com>
2024-07-29 14:07:11 +0530

    Multer and Cloundinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+  // Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret:  process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (filePath) =>{
+    try {
+        if(!filePath) return null
+        const uploadedFile = await cloudinary.uploader.upload(filePath,{
+            resource_type:"auto"  //auto will automatically detect if uploaded file is image,video,pdf etc
+        })
+
+        console.log("File have been uploaded:",uploadedFile.url)
+        return uploadedFile
+    } catch (error) {
+        fs.unlinkSync(filePath);  //remove the locally saved tempOrary file as the upload operation got failed
+        return null
+    }
+}
+
+export {uploadOnCloudinary};
